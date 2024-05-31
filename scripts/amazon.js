e9cecbf845db78bce55f8a10e7319e1544340348
Abdullah <frost1639@hotmail.com>
2024-05-31 21:32:22 +0500

    Added the logic to add message when a user clicks add to card function

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -58,6 +58,25 @@ products.forEach((product) => {
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  // This solution uses a feature of JavaScript called a
+  // closure. Each time we run the loop, it will create
+  // a new variable called addedMessageTimeoutId and do
+  // button.addEventListener().
+  //
+  // Then, because of closure, the function we give to
+  // button.addEventListener() will get a unique copy
+  // of the addedMessageTimeoutId variable and it will
+  // keep this copy of the variable forever.
+  // (Reminder: closure = if a function has access to a
+  // value/variable, it will always have access to that
+  // value/variable).
+  //
+  // This allows us to create many unique copies of the
+  // addedMessageTimeoutId variable (one for every time
+  // we run the loop) so it lets us keep track of many
+  // timeoutIds (one for each product).
+  let addedMessageTimeoutId;
+
   button.addEventListener("click", () => {
     const { productId } = button.dataset;
 
@@ -84,5 +103,26 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     });
 
     document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+    document
+      .querySelector(`.js-added-to-cart-${productId}`)
+      .classList.add("show-added-to-cart-message");
+
+    setTimeout(() => {
+      // Check if a previous timeoutId exists. If it does,
+      // we will stop it.
+      if (addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        document
+          .querySelector(`.js-added-to-cart-${productId}`)
+          .classList.remove("show-added-to-cart-message");
+      }, 2000);
+
+      // Save the timeoutId so we can stop it later.
+      addedMessageTimeoutId = timeoutId;
+    });
   });
 });
