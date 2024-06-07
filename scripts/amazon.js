7a97fcfccf06961d28934fe4eafb037f0966930a
Abdullah <frost1639@hotmail.com>
2024-06-07 23:03:10 +0500

    Save data and generate html for delivery options

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 import { addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
+import formatCurrency from "./utils/money.js";
 
 let productsHTML = "";
 
@@ -62,7 +62,7 @@ products.forEach((product) => {
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  const cartQuantity = calculateCartQuantity()
+  const cartQuantity = calculateCartQuantity();
   document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 }
 
