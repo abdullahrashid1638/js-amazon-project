6bee797d3d2e5361055593eba63d7fd5306d7a87
abdullahrashid1638 <abdullahrashid1638@gmail.com>
2024-06-22 15:44:28 +0500

    Modify updateDeliveryOption and check if productId exists in the cart

--- tests/data/cartTests.js
+++ tests/data/cartTests.js
@@ -166,4 +166,24 @@ describe("test suite: updateDeliveryOption()", () => {
       }
     ]));
   });
+
+  it ("updates the delivery option for a product that is not in the cart", () => {
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ]);
+    });
+
+    loadFromStorage();
+
+    updateDeliveryOption("not-in-cart", "3");
+
+    expect(cart[0].deliveryOptionId).toEqual("1");
+
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+  });
 });
