dba47000851ef75a08a9a1c893ee47b7f0d00a1b
abdullahrashid1638 <abdullahrashid1638@gmail.com>
2024-07-02 18:41:18 +0500

    Make a post request without body and added error handleing

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -125,14 +125,29 @@ sendJSON();
 */
 
 /*
-fetch("https://amazon.com")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("CORS error. Your request was blocked by backend");
-  });
+async function createBadRequest () {
+  try {
+    const response = await fetch("https://supersimplebackend.dev/greeting", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (response.status >= 400) throw response;
+
+    const text = await response.text();
+    console.log(text);
+
+  } catch (error) {
+    if (error.status === 400) {
+      const errorMessage = await error.json();
+      console.log(errorMessage);
+    } else {
+      console.log("Network error. Please try again later")
+    };
+  }
+}
+
+createBadRequest();
 */
